61d5a5592e00815319471a5d4703fdddd7985470
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-07-02 13:11:05 -0300

    use the routes jobs in app main

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const db = require('./db/connectDb');
+const bodyParser = require('body-parser');
 
 
 app.listen(PORT, () => {
     console.log(`The serv run at: ${PORT}`);
 });
 
+app.use(bodyParser.urlencoded({ extended: false}));
 
 //db connect:
 db
@@ -24,3 +26,6 @@ db
 app.get('/', (req, res) => {
     res.send("The serv is okay!");
 });
+
+// job routes
+app.use('/jobs', require('./routes/jobs'));
